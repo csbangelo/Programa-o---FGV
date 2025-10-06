f5de3e74ac21fd41cc81f16d80ddb00f173e4c8b
csbangelo <angello.gabryel77@gmail.com>
2025-10-06 15:22:41 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -6,6 +6,6 @@ st.write("Ronaldo")
 
 st.write("El Pepe")
 
-nome = st.text_input("Digite o seu nome:)
+nome = st.text_input("Digite o seu nome:")
 if nome
                      st.write(nome.upper())
