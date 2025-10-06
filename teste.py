b96254ae415ce7401a52d4e3d1b063e09dd54f87
csbangelo <angello.gabryel77@gmail.com>
2025-10-06 15:33:27 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -4,10 +4,6 @@ st.title('SHOW DAS PODEROSAS')
 
 st.write("Bananilson Farofa")
 
-nome1 = st.text_input("Digite o seu nome:")
+nome = st.text_input("Digite o seu nome:")
 if nome1:
-                     st.write(nome.upper())
-
-nome2 = st.text_input("Digite o seu nome:")
-if nome2:
                      st.write(nome.upper())
