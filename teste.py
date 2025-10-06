5e6de4f21688e0d396fdcc90c9294b573e9aa303
csbangelo <angello.gabryel77@gmail.com>
2025-10-06 15:32:35 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,10 +1,12 @@
 import streamlit as st
 
-st.title('Que que ele disse?')
+st.title('SHOW DAS PODEROSAS')
 
-st.write("Ronaldo")
+st.write("Bananilson Farofa")
 
-st.write("El Pepe")
+nome = st.text_input("Digite o seu nome:")
+if nome:
+                     st.write(nome.upper())
 
 nome = st.text_input("Digite o seu nome:")
 if nome:
